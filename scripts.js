26dac15468c198d0687033738f2a0615332a5340
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-01-15 14:29:23 +0600

    file javascript

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let first= "Shrek";
+let second= first;
+let third= second;
+let fourth= third;
+let fifth= fourth;
+
+let capital= "Tokio";
+let country= "Japan";
+
+console.log( capital + " is the capital of " + country);
+
