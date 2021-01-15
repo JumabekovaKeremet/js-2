441990772394501970ed3141b80926968fff0bcf
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-01-15 14:49:15 +0600

    changed console

--- scripts.js
+++ scripts.js
@@ -7,5 +7,22 @@ let fifth= fourth;
 let capital= "Tokio";
 let country= "Japan";
 
-console.log( capital + " is the capital of " + country);
+let subject= "Distance"
+let unit= "meters";
 
+subject= "Spacing";
+unit= "centimeters";
+
+subject= "Force";
+unit= "newtons";
+
+let title= "Inferno";
+let author= "Dan Brown";
+
+title= "Dead Souls";
+author= "Gogol"
+
+console.log(fifth);
+console.log(capital + " is the capital of " + country);
+console.log(subject + "is measured in "  + unit );
+console.log( "My favourite book is " + title + " by " + author + "!");
