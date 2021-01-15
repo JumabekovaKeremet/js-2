79b6ab32f1fee5f99bcdbf8195e98e4eec63a493
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-01-15 15:10:09 +0600

    changed scripts

--- scripts.js
+++ scripts.js
@@ -20,10 +20,10 @@ let title= "Inferno";
 let author= "Dan Brown";
 
 title= "Dead Souls";
-author= "Gogol"
+author= "Gogol";
 
 console.log(fifth);
 console.log(capital + " is the capital of " + country);
 console.log(subject + "is measured in "  + unit );
 console.log( "My favourite book is " + title + " by " + author + "!");
-console.log("My favourite song is \"Yesterday\" by Beatles")
+console.log("My favourite song is \"Yesterday\" by Beatles");
